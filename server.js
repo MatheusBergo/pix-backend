2f2734d12cee07b063aff4203eecb2883cdafc19
MatheusBergo <144372589+MatheusBergo@users.noreply.github.com>
2026-02-28 20:56:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post("/webhook", async (req, res) => {
     }
 
     const response = await fetch(
-      https://api.mercadopago.com/v1/payments/${paymentId},
+      `https://api.mercadopago.com/v1/payments/${paymentId}`,
       {
         method: "GET",
         headers: {
